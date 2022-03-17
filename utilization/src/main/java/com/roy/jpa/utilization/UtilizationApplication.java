35fe0dd8c0a520046b5992ebc20c206cce907251
roy-zz <roystreet91@gmail.com>
2022-03-17 22:22:42 +0900

    add new module (utilization)

---
+++ utilization/src/main/java/com/roy/jpa/utilization/UtilizationApplication.java
@@ -0,0 +1,13 @@
+package com.roy.jpa.utilization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilizationApplication.class, args);
+    }
+
+}
---
+++ utilization/src/test/java/com/roy/jpa/utilization/UtilizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roy.jpa.utilization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

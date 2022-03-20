165d362f0ae6625ce97c4d7df36f8b3fd4a02c0c
roy-zz <roystreet91@gmail.com>
2022-03-20 10:59:17 +0900

    add controllers

---
+++ utilization/src/main/java/com/roy/jpa/utilization/controller/OrderController.java
@@ -0,0 +1,53 @@
+package com.roy.jpa.utilization.controller;
+
+import com.roy.jpa.utilization.domain.Member;
+import com.roy.jpa.utilization.domain.Order;
+import com.roy.jpa.utilization.domain.item.Item;
+import com.roy.jpa.utilization.repository.OrderSearch;
+import com.roy.jpa.utilization.service.ItemService;
+import com.roy.jpa.utilization.service.MemberService;
+import com.roy.jpa.utilization.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping(value = "/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+        return "order/orderForm";
+    }
+
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping(value = "/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
+
+}

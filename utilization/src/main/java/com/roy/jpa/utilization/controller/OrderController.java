cd9c2b4d2e0f7d705a61cd33dce6cfb906bd9e67
roy-zz <roystreet91@gmail.com>
2022-03-20 11:28:21 +0900

    add db initialization script

--- utilization/src/main/java/com/roy/jpa/utilization/controller/OrderController.java
+++ utilization/src/main/java/com/roy/jpa/utilization/controller/OrderController.java
@@ -8,11 +8,13 @@ import com.roy.jpa.utilization.service.ItemService;
 import com.roy.jpa.utilization.service.MemberService;
 import com.roy.jpa.utilization.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Controller
 @RequiredArgsConstructor
 public class OrderController {
 
@@ -29,19 +31,19 @@ public class OrderController {
         return "order/orderForm";
     }
 
-    @GetMapping(value = "/orders")
-    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
-        List<Order> orders = orderService.findOrders(orderSearch);
-        model.addAttribute("orders", orders);
-        return "order/orderList";
-    }
-
     @PostMapping(value = "/order")
     public String order(@RequestParam("memberId") Long memberId,
                         @RequestParam("itemId") Long itemId,
                         @RequestParam("count") int count) {
         orderService.order(memberId, itemId, count);
         return "redirect:/orders";
+    }
+
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
     }
 
     @PostMapping("/orders/{orderId}/cancel")
